b57e5d4c045b9895e9e74bf4e32d48acb5a78210
John Aquino <johnaquino@outlook.com>
2024-04-30 16:30:28 -0400

    More changes

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,5 +1,6 @@
-import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { LambdaService } from '../../../services/lambda.service';
+import { RepoSelectionService } from '../../../services/repo-selection.service';
 
 @Component({
   selector: 'app-card',
@@ -8,39 +9,97 @@ import { Component, Input } from '@angular/core';
   styleUrl: './card.component.scss',
 })
 export class CardComponent {
-  @Input() cardTitle: string = '';
-  
-  @Input() cardContent: any;
-  constructor() {
-    
+  @Input() cardType: string = '';
+  cardTitle: string = '';
+  isLoaded = false;
+
+  cardContent: any;
+  constructor(private repoSelectionService: RepoSelectionService, private lambdaService: LambdaService) {
+
   }
 
   ngOnInit(): void {
     console.log(this.cardContent);
+
+    switch (this.cardType) {
+      case 'overview':
+        this.cardTitle = 'Overview';
+        // repoSelectionService
+        break;
+      case 'security':
+        this.cardTitle = 'Security';
+
+        if (this.repoSelectionService.getSecurityRecommendation() != '') {
+          this.cardContent = this.repoSelectionService.getSecurityRecommendation();
+          this.isLoaded = true;
+        }
+
+        this.repoSelectionService.securityRecommendation$.subscribe((cardContent) => {
+          this.cardContent = cardContent;
+          this.isLoaded = true;
+        });
+
+        break;
+      case 'performance':
+        this.cardTitle = 'Performance';
+        break;
+      case 'quality':
+        this.cardTitle = 'Quality';
+
+        if (this.repoSelectionService.getCodeQualityRecommendation() != '') {
+          this.cardContent = this.repoSelectionService.getCodeQualityRecommendation();
+          this.isLoaded = true;
+        }
+
+        this.repoSelectionService.codeQualityRecommendation$.subscribe((cardContent) => {
+          this.cardContent = cardContent;
+          this.isLoaded = true;
+        });
+        break;
+      case 'optimization':
+        this.cardTitle = 'Optimization';
+
+        if (this.repoSelectionService.getOptimizationRecommendation() != '') {
+          this.cardContent = this.repoSelectionService.getOptimizationRecommendation();
+          this.isLoaded = true;
+        }
+
+        this.repoSelectionService.optimizationRecommendation$.subscribe((cardContent) => {
+          this.cardContent = cardContent;
+          this.isLoaded = true;
+        });
+        break;
+    }
+
   }
 
-  mockWriterForOverview(component: { name: string; description: string; }, cardContent: { value: { architecture: {
-    high_level_components: any; data_flow: string; }; }; }) {
+  mockWriterForOverview(component: { name: string; description: string; }, cardContent: {
+    value: {
+      architecture: {
+        high_level_components: any; data_flow: string;
+      };
+    };
+  }) {
     let html = '';
     html += '\<h3\>Overview\</h3\>';
     html += '\<h4\>Architecture\</h4\>';
-    html +='\<ul\>';
+    html += '\<ul\>';
     for (let component of cardContent.value.architecture.high_level_components) {
       html += '\<li\>' + component.name + ':\</strong\> ' + component.description + '\</li\>';
     }
-  
-    html +='\</ul\>';
-    html += '\<p\>\<strong\>Data Flow:\</strong\>'+  cardContent.value.architecture.data_flow + '\</p\>';
+
+    html += '\</ul\>';
+    html += '\<p\>\<strong\>Data Flow:\</strong\>' + cardContent.value.architecture.data_flow + '\</p\>';
     return html;
   }
 
   mockWriterForSecurity(cardContent: any): string {
     let html = '';
-  
+
     html += '<h3>Overview</h3>';
     html += '<h4>Security</h4>';
     html += '<ul>';
-  
+
     for (let vulnerability of cardContent.vulnerabilities) {
       html += '<li>';
       html += '<strong>Description:</strong> ' + vulnerability.description;
@@ -53,19 +112,19 @@ export class CardComponent {
       html += '<strong>Severity:</strong> ' + vulnerability.severity;
       html += '</li>';
     }
-  
+
     html += '</ul>';
-  
+
     return html;
   }
-  
+
   mockWriterForPerformance(cardContent: any): string {
     let html = '';
-  
+
     html += '<h3>Overview</h3>';
     html += '<h4>Performance</h4>';
     html += '<ul>';
-  
+
     if (cardContent.issues.length > 0) {
       html += '<strong>Issues:</strong>';
       for (let issue of cardContent.issues) {
@@ -74,46 +133,46 @@ export class CardComponent {
     } else {
       html += '<li>No issues found</li>';
     }
-  
+
     html += '</ul>';
-  
+
     return html;
   }
 
   mockWriterForCodeAnalysis(cardContent: any): string {
     let html = '';
-  
+
     html += '<h3>Code Analysis</h3>';
     html += '<h4>Classes</h4>';
     html += '<ul>';
-  
+
     for (let classObj of cardContent.classes) {
       html += '<li>';
       html += '<strong>' + classObj.name + ':</strong>';
       html += '<ul>';
-  
+
       for (let method of classObj.methods) {
         html += '<li>';
         html += '<strong>' + method.name + ':</strong> ' + method.description;
         html += '</li>';
       }
-  
+
       html += '</ul>';
       html += '</li>';
     }
-  
+
     html += '</ul>';
-  
+
     return html;
   }
 
   mockWriterForFiles(cardContent: any[]): string {
     let html = '';
-  
+
     html += '<h3>Code Analysis</h3>';
     html += '<h4>Files</h4>';
     html += '<ul>';
-  
+
     for (let file of cardContent) {
       html += '<li>';
       html += '<strong>Path:</strong> ' + file.path + '<br>';
@@ -121,9 +180,9 @@ export class CardComponent {
       html += '<strong>Size:</strong> ' + file.size + ' lines of code.<br>';
       html += '</li>';
     }
-  
+
     html += '</ul>';
-  
+
     return html;
   }
 }
