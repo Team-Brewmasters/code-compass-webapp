79ccb15c890f896ae0ec31bc527dfde2bbc8ad05
John Aquino <johnaquino@outlook.com>
2024-04-30 20:39:54 -0400

    Update typewriter directive to parse markdown before applying typewriter effect

--- src/app/directives/typwriter.directive.ts
+++ src/app/directives/typwriter.directive.ts
@@ -8,23 +8,29 @@ export class TypewriterDirective implements OnChanges {
     @Input('appTypewriter') text: string = '';
     @Input() delay: number = 50;
 
-    private parsedHtml: string = '';
-
     constructor(private el: ElementRef, private markdownService: MarkdownService) { }
 
     ngOnChanges(changes: SimpleChanges) {
         if (changes['text']) {
             this.el.nativeElement.innerHTML = '';
-            this.parsedHtml = this.markdownService.parse(changes['text'].currentValue);
-            this.typewriterEffect();
+            this.parseMarkdown(changes['text'].currentValue);
         }
     }
 
-    typewriterEffect() {
+    private async parseMarkdown(text: string) {
+        try {
+            const parsedHtml = await this.markdownService.parse(text);
+            this.typewriterEffect(parsedHtml);
+        } catch (error) {
+            console.error('Error parsing markdown:', error);
+        }
+    }
+
+    private typewriterEffect(parsedHtml: string) {
         let i = 0;
         const typing = () => {
-            if (i < this.parsedHtml.length) {
-                const currentHtml = this.parsedHtml.slice(0, i + 1);
+            if (i < parsedHtml.length) {
+                const currentHtml = parsedHtml.slice(0, i + 1);
                 this.el.nativeElement.innerHTML = currentHtml;
                 i++;
                 setTimeout(typing, this.delay);
